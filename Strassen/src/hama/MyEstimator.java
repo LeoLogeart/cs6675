22df7e06c3d126a49f84bf60ab1bf2a9e113ab37
leo <leo.logeart@gmail.com>
2014-03-25 11:59:28 -0400

    Test of Pi estimators

---
+++ Strassen/src/hama/MyEstimator.java
@@ -0,0 +1,11 @@
+package hama;
+import java.io.IOException;
+
+import org.apache.hama.examples.PiEstimator;
+
+public class MyEstimator {
+	public static void main(String[] args) throws IOException,
+			InterruptedException, ClassNotFoundException {
+		PiEstimator.main(args);
+	}
+}
