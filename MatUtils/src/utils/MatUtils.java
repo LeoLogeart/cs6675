f25760d62be88b9cfe3bf44ace6db2a9a49b5bd6
leo <leo.logeart@gmail.com>
2014-04-10 13:54:58 -0400

    new project MatUtils to compare matrices, read a matrix, run an empty hama job, and generate random matrices

---
+++ MatUtils/src/utils/MatUtils.java
@@ -0,0 +1,117 @@
+package utils;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hama.HamaConfiguration;
+import org.apache.hama.bsp.BSP;
+import org.apache.hama.bsp.BSPJob;
+import org.apache.hama.bsp.BSPPeer;
+import org.apache.hama.bsp.sync.SyncException;
+
+public class MatUtils {
+	public static class NullBSP
+			extends
+			BSP<NullWritable, NullWritable, NullWritable, NullWritable, NullWritable> {
+
+		@Override
+		public void setup(
+				BSPPeer<NullWritable, NullWritable, NullWritable, NullWritable, NullWritable> peer)
+				throws IOException, SyncException, InterruptedException {
+			peer.sync();
+		}
+
+		@Override
+		public void bsp(
+				BSPPeer<NullWritable, NullWritable, NullWritable, NullWritable, NullWritable> peer)
+				throws IOException, SyncException, InterruptedException {
+			peer.sync();
+		}
+
+		@Override
+		public void cleanup(
+				BSPPeer<NullWritable, NullWritable, NullWritable, NullWritable, NullWritable> peer)
+				throws IOException {
+		}
+	}
+
+	public static void main(String[] args) {
+		if (args[0].equals("read")) {
+			double[][] cValues = Utils.readMatrix(new Path(args[1]),
+					new HamaConfiguration(), Integer.parseInt(args[2]),
+					Integer.parseInt(args[3]), 1);
+			Matrix c = new Matrix(cValues, Integer.parseInt(args[2]),
+					Integer.parseInt(args[3]));
+			c.print();
+		} else if (args[0].equals("gen")) {
+			int rowSize = Integer.parseInt(args[1]);
+			int colSize = Integer.parseInt(args[2]);
+			Path outputPath = new Path(args[3]);
+
+			double[][] matrix = MatrixGenerator.createRandomMatrix(rowSize,
+					colSize, new Random(), 0, 100);
+			MatrixGenerator.writeMatrix(matrix, outputPath,
+					new HamaConfiguration());
+		} else if (args[0].equals("cmp")) {
+			int rows = Integer.parseInt(args[3]);
+			double[][] aValues = Utils
+					.readMatrix(new Path(args[1]), new HamaConfiguration(),
+							rows, Integer.parseInt(args[4]), 1);
+
+			double[][] bValues = Utils
+					.readMatrix(new Path(args[2]), new HamaConfiguration(),
+							rows, Integer.parseInt(args[4]), 1);
+			boolean eq = true;
+			int i = 0;
+			while (i < rows && eq) {
+				Arrays.equals(aValues[i], bValues[i]);
+				i++;
+			}
+			if (eq) {
+				System.out.println("Matrices match");
+			} else {
+				System.out.println("Matrices do not match");
+			}
+		} else if (args[0].equals("hamaSetup")) {
+			HamaConfiguration conf = new HamaConfiguration();
+			BSPJob bsp;
+			try {
+				bsp = new BSPJob(conf);
+
+				// Set the job name
+				bsp.setJobName("Empty job");
+				bsp.setBspClass(NullBSP.class);
+				bsp.setJar("matUtils.jar");
+				bsp.setOutputPath(new Path("tmp"));
+				bsp.setNumBspTask(Integer.parseInt(args[1]));
+
+				long startTime = System.currentTimeMillis();
+				if (bsp.waitForCompletion(true)) {
+					System.out.println("Running Hama took "
+							+ (System.currentTimeMillis() - startTime) / 1000.0
+							+ " seconds.");
+				}
+			} catch (ClassNotFoundException | IOException
+					| InterruptedException e) {
+				e.printStackTrace();
+			}
+		} else {
+			printUsage();
+		}
+	}
+
+	private static void printUsage() {
+		System.out.println("Usage :");
+		System.out
+				.println("hama -jar utils.jar read <path to matrix> <rows size> <column size>");
+		System.out
+				.println("hama -jar utils.jar gen <row size> <column size> <output path>");
+		System.out
+				.println("hama -jar utils.jar cmp <path matrix 1> <path matrix 2> <mat rows size> <mat column size>");
+		System.out.println("hama -jar utils.jar hamaSetup <number of nodes>");
+	}
+
+}
---
+++ MatUtils/src/utils/MatrixGenerator.java
@@ -0,0 +1,74 @@
+package utils;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Random;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hama.HamaConfiguration;
+import org.apache.hama.commons.io.VectorWritable;
+import org.apache.hama.commons.math.DenseDoubleVector;
+
+// Generates random matrix and stores it as sequence file in dfs
+public class MatrixGenerator {
+
+	public static void main(String[] args) throws InterruptedException,
+			IOException, ClassNotFoundException {
+
+		int rowSize = Integer.parseInt(args[0]);
+		int colSize = Integer.parseInt(args[1]);
+		Path outputPath = new Path(args[2]);
+
+		double[][] matrix = createRandomMatrix(rowSize, colSize, new Random(),
+				0, 100);
+
+		// double[][] a={{1,2,3},{2,0,1},{1,1,1},{2,2,0}};
+		// double[][] b={{1,2},{0,1},{1,1}};
+		writeMatrix(matrix, outputPath, new HamaConfiguration());
+	}
+
+	public static double[][] createRandomMatrix(int rows, int columns,
+			Random rand, double rangeMin, double rangeMax) {
+
+		double[][] matrix = new double[rows][columns];
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				double randomValue = rangeMin + (rangeMax - rangeMin)
+						* rand.nextDouble();
+
+				matrix[i][j] = new BigDecimal(randomValue).doubleValue();
+			}
+		}
+		return matrix;
+	}
+
+	// Stores matrix as sequence file in dfs
+	public static Path writeMatrix(double[][] matrix, Path path,
+			HamaConfiguration conf) {
+		SequenceFile.Writer writer = null;
+		try {
+			FileSystem fs = FileSystem.get(conf);
+			writer = new SequenceFile.Writer(fs, conf, path, IntWritable.class,
+					VectorWritable.class);
+			for (int i = 0; i < matrix.length; i++) {
+				DenseDoubleVector rowVector = new DenseDoubleVector(matrix[i]);
+				writer.append(new IntWritable(i), new VectorWritable(rowVector));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return path;
+	}
+}
