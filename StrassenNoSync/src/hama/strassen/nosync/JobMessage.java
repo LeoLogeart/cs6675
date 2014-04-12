3c0de070b388513741779dc32bf03c7bc61da02f
RLem <rabablembachar@gmail.com>
2014-04-12 18:03:58 -0400

    Strassen without sync

---
+++ StrassenNoSync/src/hama/strassen/nosync/JobMessage.java
@@ -0,0 +1,86 @@
+package hama.strassen.nosync;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+public class JobMessage implements Writable{
+	
+	private int type; //O for job 1 for res
+	private int i;
+	private int j;
+	private int k;
+	
+	private int sender;
+	private Matrix result;
+
+	public JobMessage() {
+		super();
+	}
+
+	public JobMessage(int type, int i, int j, int k) {
+		super();
+		this.type = type;
+		this.i = i;
+		this.j = j;
+		this.k = k;
+	}
+	
+	public JobMessage(int type,int sender, Matrix result) {
+		this.type = type;
+		this.sender = sender;
+		this.result = result;
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		type = in.readInt();
+		if (type==0) {
+			i = in.readInt();
+			j = in.readInt();
+			k = in.readInt();
+		} else if (type==1){
+			sender = in.readInt();
+			result = DoubleMatrixWritable.read(in);
+		}
+		
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		if (type==0){
+			out.writeInt(type);
+			out.writeInt(i);
+			out.writeInt(j);
+			out.writeInt(k);
+		} else if (type==1){
+			out.writeInt(type);
+			out.writeInt(sender);
+			DoubleMatrixWritable.write(result, out);
+		}
+		
+	}
+	
+	public int getSender() {
+		return sender;
+	}
+
+	public Matrix getResult() {
+		return result;
+	}
+
+
+	public int getI() {
+		return i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+	public int getK() {
+		return k;
+	}
+}
---
+++ StrassenNoSync/src/hama/strassen/nosync/StrassenMultiply.java
@@ -0,0 +1,209 @@
+package hama.strassen.nosync;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hama.HamaConfiguration;
+import org.apache.hama.bsp.BSP;
+import org.apache.hama.bsp.BSPJob;
+import org.apache.hama.bsp.BSPPeer;
+import org.apache.hama.bsp.sync.SyncException;
+
+public class StrassenMultiply {
+	private static final String inputMatrixAPathString = "strassen.patha";
+	private static final String inputMatrixBPathString = "strassen.pathb";
+	private static final String inputMatrixCPathString = "strassen.pathc";
+	private static final String blockSizeString = "strassen.blocksize";
+	private static final String inputMatrixARows = "strassen.nbRowA";
+	private static final String inputMatrixACols = "strassen.nbColA";
+	private static final String inputMatrixBRows = "strassen.nbRowB";
+	private static final String inputMatrixBCols = "strassen.nbColB";
+
+	public static class StrassenBSP
+			extends
+			BSP<NullWritable, NullWritable, NullWritable, NullWritable, JobMessage> {
+
+		private int nbRowsA;
+		private int nbColsA;
+		private int nbRowsB;
+		private int nbColsB;
+		private int blockSize = 2;
+		private Matrix a;
+		private Matrix b;
+
+		@Override
+		public void setup(
+				BSPPeer<NullWritable, NullWritable, NullWritable, NullWritable, JobMessage> peer)
+				throws IOException, SyncException, InterruptedException {
+			HamaConfiguration conf = peer.getConfiguration();
+			blockSize = conf.getInt(blockSizeString, 2);
+
+			/* Values for rows and columns padded */
+			nbRowsA = Utils.getBlockMultiple(conf.getInt(inputMatrixARows, 4),
+					blockSize);
+			nbColsA = Utils.getBlockMultiple(conf.getInt(inputMatrixACols, 4),
+					blockSize);
+			nbRowsB = Utils.getBlockMultiple(conf.getInt(inputMatrixBRows, 4),
+					blockSize);
+			nbColsB = Utils.getBlockMultiple(conf.getInt(inputMatrixBCols, 4),
+					blockSize);
+			a = new Matrix(Utils.readMatrix(
+					new Path(conf.get(inputMatrixAPathString)),
+					peer.getConfiguration(), conf.getInt(inputMatrixARows, 4),
+					conf.getInt(inputMatrixACols, 4), blockSize), nbRowsA,
+					nbColsA);
+			b = new Matrix(Utils.readMatrix(
+					new Path(conf.get(inputMatrixBPathString)),
+					peer.getConfiguration(), conf.getInt(inputMatrixBRows, 4),
+					conf.getInt(inputMatrixBCols, 4), blockSize), nbRowsB,
+					nbColsB);
+		}
+
+		@Override
+		public void bsp(
+				BSPPeer<NullWritable, NullWritable, NullWritable, NullWritable, JobMessage> peer)
+				throws IOException, SyncException, InterruptedException {
+			int nbRowsBlocks = nbRowsA / blockSize;
+			int nbColsBlocks = nbColsB / blockSize;
+			int nbBlocks = nbRowsBlocks * nbColsBlocks;
+			int blocksPerPeer = nbBlocks / peer.getNumPeers();
+			int p = peer.getPeerIndex();
+			int lastBlock = (p+1)*blocksPerPeer;
+			if (p==peer.getNumPeers()-1) {
+				lastBlock = nbBlocks;
+			}
+			// Peer p gets blocks p*blocksPerPeer -> (p+1)blocksPerPeer
+			for (int block = p*blocksPerPeer; block<lastBlock; block++){
+				int i = block/nbColsBlocks;
+				int j = block%nbColsBlocks;
+				Matrix resBlock = new Matrix(blockSize, blockSize);
+				resBlock.zeroes();
+				for (int k = 0; k < nbRowsBlocks; k++) {
+					Matrix aBlock = a.getBlock(i, k, blockSize);
+					Matrix bBlock = b.getBlock(k, j, blockSize);
+					resBlock = resBlock.sum(aBlock.strassen(bBlock));
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		HamaConfiguration conf = new HamaConfiguration();
+		BSPJob bsp = new BSPJob(conf);
+		// Set the job name
+		bsp.setJobName("Strassen Multiply");
+		bsp.setBspClass(StrassenBSP.class);
+		bsp.setJar("strassen.jar");
+
+		if (args.length < 6 || args.length > 10) {
+			printUsage();
+			return;
+		} else {
+			if (parseArgs(args, conf, bsp) != 0) {
+				return;
+			}
+		}
+
+		long startTime = System.currentTimeMillis();
+		if (bsp.waitForCompletion(true)) {
+			System.out.println("Job Finished in "
+					+ (System.currentTimeMillis() - startTime) / 1000.0
+					+ " seconds.");
+		}
+		/*
+		double[][] cValues = Utils.readMatrix(new Path(args[7]), conf,
+				Integer.parseInt(args[1]), Integer.parseInt(args[5]),
+				Integer.parseInt(args[9]));
+		Matrix c = new Matrix(cValues, Integer.parseInt(args[1]),
+				Integer.parseInt(args[5]));
+		c.print();*/
+
+	}
+
+	private static int parseArgs(String[] args, HamaConfiguration conf,
+			BSPJob bsp) {
+		conf.set(inputMatrixAPathString, args[0]);
+		conf.setInt(inputMatrixARows, Integer.parseInt(args[1]));
+		conf.setInt(inputMatrixACols, Integer.parseInt(args[2]));
+		conf.set(inputMatrixBPathString, args[3]);
+		conf.setInt(inputMatrixBRows, Integer.parseInt(args[4]));
+		if (Integer.parseInt(args[2]) != Integer.parseInt(args[4])) {
+			System.out.println("Matrices size do not match.");
+			return 1;
+		}
+		conf.setInt(inputMatrixBCols, Integer.parseInt(args[5]));
+		bsp.setOutputPath(new Path(args[6]));
+		conf.set(inputMatrixCPathString, args[7]);
+		if (args.length > 8) {
+			bsp.setNumBspTask(Integer.parseInt(args[8]));
+			if (args.length > 9) {
+				int n = Integer.parseInt(args[9]);
+				if ((n & (n - 1)) != 0) {
+					System.out.println("The block size must be a power of two");
+					return 1;
+				}
+				conf.setInt(blockSizeString, n);
+			} else {
+				/*
+				 * set the size of blocks depending on the number of peers and
+				 * matrices sizes
+				 */
+				/*
+				 * let's assume square matrices : nbTasks =
+				 * (PaddedRowSize/sizeBlock)^3 ,
+				 */
+				int rows = conf.getInt(inputMatrixARows, 4);
+				int n = 2;
+				/* set n as the largest power of two smaller than row/2 */
+				while (n < rows / 2) {
+					n *= 2;
+				}
+				n /= 2;
+
+				int nbPeers = Integer.parseInt(args[8]);
+				/*
+				 * lower the block size to make sure every peer has at least a
+				 * task
+				 */
+				while (Math.pow(rows / n, 3) < nbPeers) {
+					n /= 2;
+				}
+				/* taking the future padding into account */
+				n *= 2;
+				System.out.println("block size :" + n);
+				conf.setInt(blockSizeString, n);
+			}
+		} else {
+			/*
+			 * set the size of blocks depending on the number of peers and
+			 * matrices sizes
+			 */
+			/*
+			 * let's assume square matrices : nbTasks =
+			 * (PaddedRowSize/sizeBlock)^3 ,
+			 */
+			int rows = conf.getInt(inputMatrixARows, 4);
+			int n = 2;
+			/* set n as the largest power of two smaller than row/4 */
+			while (n < rows / 4) {
+				n *= 2;
+			}
+			conf.setInt(blockSizeString, n);
+			int tasks = (int) Math.pow((int) (rows / n), 3);
+			System.out.println("block size :" + n);
+			System.out.println("peers :" + tasks);
+			bsp.setNumBspTask(tasks);
+		}
+		return 0;
+	}
+
+	private static void printUsage() {
+		System.out
+				.println("Usage: StrassenMultiply <path A> <number rows A> <number columns A>"
+						+ "<path B> <number rows B> <number columns B> <path output> <path C> [number of tasks] [block size]");
+	}
+}
